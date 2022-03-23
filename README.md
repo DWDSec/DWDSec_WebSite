9b154f4f8e44206efadad2eddf15cf5013d019c5
Yaoisss <3136586731@qq.com>
2022-03-23 20:41:48 +0800

    update

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 This is DWDSEC Network Security Laboratory
 
-TIP:
+安装流程:
 
 ```bash
+//1.安装node.js
+//2.安装yarn
+
+npm install --global yarn
+
+//3.安装vuepress
+
 yarn add -D vuepress@next
+
+//4.运行官网
+yarn docs:dev
+
 ```
+
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //端口
+    port: '80',
+    // 站点配置
+    lang: 'zh-CN',
+    title: 'DWDSEC网络安全实验室',
+    description: '这里是DWDSEC网络安全实验室的自留地。',
+  
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+      logo: 'https://vuejs.org/images/logo.png',
+      sidebar: 'auto'
+    }
+  }
