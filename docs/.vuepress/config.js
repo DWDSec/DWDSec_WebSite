c968a666804b0c21113920a8e4c2e214f1025709
Crazy4Tech <80889440+Crazy4Tech@users.noreply.github.com>
2022-09-14 15:24:36 +0800

    add XFAN, brown and dreamer

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,38 +1,41 @@
 module.exports = {
-    //端口
-    port: '80',
-    // 站点配置
-    lang: 'zh-CN',
-    title: 'DWDSec网络安全实验室',
-    description: 'Defend With Determination',
-  
-    // 主题和它的配置
-    theme: '@vuepress/theme-default',
-    themeConfig: {
-      logo: '/member_avatar/DWDSEC_LOGO.png',
-      nav: [
-        { text: '实验室文章', link: '/articles/' },
-        { text: '实验室项目', link: '/projects/' },
-        { text: '实验室成员', link: '/members/' }
-      ],
-      sidebar: {
-        "/members/":[
-          '/members/',
-          'Yaoisss',
-          'TXPH',
-          'SmallL_U',
-          'Juana_2u',
-          'nLesxw',
-          '808',
-          'KRISS-HDB',
-          'HeyAspirin',
-          'QingNiao',
-          'XIAO',
-          'Crazy4Tech',
-          'TerryTao',
-          '@'
-        ]
-      },
-      sidebarDepth: 2
-    }
+  //端口
+  port: '80',
+  // 站点配置
+  lang: 'zh-CN',
+  title: 'DWDSec网络安全实验室',
+  description: 'Defend With Determination',
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/member_avatar/DWDSEC_LOGO.png',
+    nav: [
+      { text: '实验室文章', link: '/articles/' },
+      { text: '实验室项目', link: '/projects/' },
+      { text: '实验室成员', link: '/members/' }
+    ],
+    sidebar: {
+      '/members/': [
+        '',
+        'Yaoisss',
+        'TXPH',
+        'SmallL_U',
+        'Juana_2u',
+        'nLesxw',
+        '808',
+        'KRISS-HDB',
+        'HeyAspirin',
+        'QingNiao',
+        'XIAO',
+        'Crazy4Tech',
+        'TerryTao',
+        '@',
+        'XFAN',
+        'brown',
+        'dreamer'
+      ]
+    },
+    sidebarDepth: 2
   }
+}
